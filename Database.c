861607e0e70797d45f56356231fc00d9de81bffb
Rafael Simarmata <Rafaelsimarmata23@gmail.com>
2025-06-22 15:51:39 +0700

    Update Database.c

--- Database.c
+++ Database.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct {
+char nrp[11];
+char nama[26];
+int gender;
+float ipk;
+} Mahasiswa;
+
+void tambah_data(){
+FILE *f = fopen("data.bin","ab");
+  if (!f){
+  printf("Error 404(Not Found).\n");
+  return;
+}
+
+Mahasiswa mhs;
+printf("NRP                 : "); scanf(" %[^\n]", mhs.nrp);
+printf("Nama                : "); scanf(" %[^\n]", mhs.nama);
+printf("Gender    (0=L/1=P) : "); scanf("%d", &mhs.gender);
+printf("IPK                 : "); scanf("%f", &mhs.ipk);
+
+fwrite(&mhs, sizeof(Mahasiswa),1,f);
+fclose(f);
+printf("✓Data Ditambahkan\n");
+}
