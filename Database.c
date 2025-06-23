5bc0403385d32729f31b70b029bad5656ce9a3b3
Farrel Jatmiko Aji <176496434+Miko-a@users.noreply.github.com>
2025-06-23 14:08:12 +0700

    Update hapus_data() Database.c

--- Database.c
+++ Database.c
@@ -5,17 +5,19 @@
 #include <conio.h>
 
 typedef struct {
-char nrp[11];
-char nama[26];
-int gender;
-float ipk;
+    char nrp[11];
+    char nama[26];
+    int gender;
+    float ipk;
 } Mahasiswa;
+
 
 void tambah_data(){
     FILE *f = fopen("data.bin","ab");
+    
     if (!f){
-    printf("Error 404(Not Found).\n");
-    return;
+        printf("Error 404(Not Found).\n");
+        return;
     }
 
     Mahasiswa mhs;
@@ -26,12 +28,14 @@ void tambah_data(){
 
     fwrite(&mhs, sizeof(Mahasiswa),1,f);
     fclose(f);
-    printf("✓Data Ditambahkan\n");
+    
+    printf("✓ Data Ditambahkan\n");
+    printf("Tekan tombol apapun untuk melanjutkan...\n");
     getch();
 }
 
 void ubah_data() {
- FILE *f = fopen("data.bin", "rb+");
+    FILE *f = fopen("data.bin", "rb+");
     if (!f) {
         printf("Gagal membuka file.\n");
         return;
@@ -49,31 +53,81 @@ void ubah_data() {
             printf("Gender (0=L/1=P)    : "); scanf("%d", &mhs.gender);
             printf("IPK                 : "); scanf("%f", &mhs.ipk);
 
-        fseek(f, -(long)sizeof(Mahasiswa), SEEK_CUR);
-        fwrite(&mhs, sizeof(Mahasiswa), 1, f);
-        printf("Data Berhasil Diubah\n");
-        getch();
-        found = 1;
-        break;
+            fseek(f, -(long)sizeof(Mahasiswa), SEEK_CUR);
+            fwrite(&mhs, sizeof(Mahasiswa), 1, f);
+            printf("Data Berhasil Diubah\n");
+            getch();
+            found = 1;
+            break;
+        }
     }
-}
-    if (!found) {
-    printf("NRP tidak ditemukan.\n");
-    getch();
+        if (!found) {
+            printf("NRP tidak ditemukan.\n");
+            printf("Tekan tombol apapun untuk melanjutkan...\n");
+            getch();
+        }
+        fclose(f);
     }
-    fclose(f);
-}
-}
 
-void tampil_data(int idx) {
+
+void tampil_data(bool idx_flag) {
 
 }
 
 void hapus_data() {
+    FILE *f = fopen("data.bin", "rb");
+    if (!f) {
+        printf("Gagal membuka file untuk membaca..\n");
+        return;
+    }
 
+    // hitung jumlah data
+    int count = 0;
+    Mahasiswa list[100];
+    while(fread(&list[count], sizeof(Mahasiswa), 1, f )) {
+        count++;
+    }
+    fclose(f);
+
+    char nrp[11];
+    printf("Masukkan NRP yang ingin dihapus: ");
+    scanf(" %[^\n]", nrp);
+    
+    int found = 0;
+    for (int i = 0; i < count; i++) {
+        if (strcmp(list[i].nrp, nrp) == 0) {
+            for (int j = i; j < count; j++) {
+                list[j] = list[j+1];        // shifting
+            }
+            count--;
+            found = 1;            
+        }
+    }
+
+    if (!found) {
+        printf("NRP tidak ditemukan.\n");
+        printf("Tekan tombol apapun untuk melanjutkan...\n");
+        getch();
+        return;
+    }
+
+    // tulis ulang
+    f = fopen("data.bin", "wb");
+    if (!f) {
+        printf("Gagal membuka file untuk menulis..\n");
+        return;
+    }
+
+    fwrite(list, sizeof(Mahasiswa), count, f);
+
+    printf("Data Berhasil dihapus.\n");
+    printf("Tekan tombol apapun untuk melanjutkan...\n");
+    getch();
 }
 
 int main() {
+    load();
+
     int pilihan = 0;
     char key;
 
@@ -117,7 +171,7 @@ int main() {
                 case 2: tampil_data(1); break;  // dengan index
                 case 3: tampil_data(0); break;  // tanpa index
                 case 4: hapus_data(); break;
-                case 5: return;
+                case 5: break;
             }
         }
     }
